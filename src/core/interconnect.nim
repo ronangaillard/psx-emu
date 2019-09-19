c79201af13e3cfdcabc681159403cd4182309e90
Ronan Gaillard <ronan.gaillard@live.fr>
2019-09-20 00:12:44 +0200

    Prevent unaligned memory access

--- src/core/interconnect.nim
+++ src/core/interconnect.nim
@@ -1,7 +1,9 @@
 import memoryzone
 import strformat
 
-type UnallocatedAddress = Exception
+type 
+  UnallocatedAddress = Exception
+  UnalignedMemoryAccess = Exception
 
 type
   Interconnect* = object
@@ -11,6 +13,9 @@ proc addZone*(this: var Interconnect, mz: MemoryZone) =
   this.memoryZones.add(mz)
 
 proc load32*(this: Interconnect, address: uint32): uint32 =
+  if address mod 4 != 0:
+    raise newException(UnalignedMemoryAccess, "Unaligned memory access is not allowed")
+
   for mz in this.memoryZones:
     if mz.provides(address):
       return mz.load32(address)
@@ -18,6 +23,9 @@ proc load32*(this: Interconnect, address: uint32): uint32 =
   raise newException(UnallocatedAddress, fmt"Address {address} is unallocated")
 
 proc store32*(this: Interconnect, address: uint32, value:uint32) =
+  if address mod 4 != 0:
+    raise newException(UnalignedMemoryAccess, "Unaligned memory access is not allowed")
+    
   for mz in this.memoryZones:
     if mz.provides(address):
       mz.store32(address, value)
